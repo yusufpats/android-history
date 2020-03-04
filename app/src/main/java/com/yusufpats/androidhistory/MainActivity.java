cf0beb302a21f7385cf5d971e8f866688458d8b2
Yusuf Patrawala <yusuf.patrawala@ignitesol.com>
2020-03-04 16:34:03 +0530

    Add TODO items to the MainActivity class file

--- app/src/main/java/com/yusufpats/androidhistory/MainActivity.java
+++ app/src/main/java/com/yusufpats/androidhistory/MainActivity.java
@@ -10,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO: STEP-1: Bind the recycler view element object to java code
+
+        // TODO: STEP-2: Create a LayoutManager and attach it to the recycler view object
+
+        // TODO: STEP-3: Create an object of the RecyclerView.Adapter class and attach it to the recycler view
+
     }
 }
