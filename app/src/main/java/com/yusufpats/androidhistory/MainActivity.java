d06d3baec5a0db77925a10b81fe7e44ef6b645ca
Yusuf Patrawala <yusuf.patrawala@ignitesol.com>
2020-03-04 17:11:36 +0530

    STEP-2: Create and attach a layout manager to recycler view

--- app/src/main/java/com/yusufpats/androidhistory/MainActivity.java
+++ app/src/main/java/com/yusufpats/androidhistory/MainActivity.java
@@ -3,6 +3,7 @@ package com.yusufpats.androidhistory;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,7 +19,9 @@ public class MainActivity extends AppCompatActivity {
         // STEP-1: Bind the recycler view element to java object
         historyRecyclerView = findViewById(R.id.history_recycler_view);
 
-        // TODO: STEP-2: Create a LayoutManager and attach it to the recycler view object
+        // STEP-2: Create a LayoutManager and attach it to the recycler view object
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        historyRecyclerView.setLayoutManager(layoutManager);
 
         // TODO: STEP-3: Create an object of the RecyclerView.Adapter class and attach it to the recycler view
 
